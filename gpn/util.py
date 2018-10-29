08d79b5c85becab8a5090d282ac40d90067888fc
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-10-29 13:11:51 -0400

    Return  batch with arrays instead of persisting to hdf5
     - this works now
     - no unnecessary write/read
     - duplicate arrays as needed

--- gpn/util.py
+++ gpn/util.py
@@ -1,36 +1,28 @@
-import os
-
-from gunpowder import ArrayKey, BatchRequest, Normalize, Pad, Crop, \
-    build, Hdf5Write, RandomLocation, RandomProvider
+from gpn.duplicate import Duplicate
+from gunpowder import ArrayKey, BatchRequest, build, RandomLocation, RandomProvider, ArraySpec
 
 RAW       = ArrayKey('RAW')
 GT_LABELS = ArrayKey('GT_LABELS')
-
 
 def run_augmentations(
         data_providers,
         roi,
         keys_with_sizes=(),
         augmentations=(),
-        store_inputs_node=None,
-        output_node=Hdf5Write(dataset_names={RAW:'volumes/raw'}, output_dir=os.path.join(os.path.expanduser("~"), "tmp"), output_filename="test-nodes.h5")):
-
-
+        duplicate_key_mapping=lambda key: '{}-original'.format(key.identifier)):
 
     request = BatchRequest()
+    duplicates = {}
     for key, size in keys_with_sizes:
-        request.add(key, roi(key).get_shape())
+        request[key] = ArraySpec(roi(key))
+        duplicates[key] = ArrayKey(duplicate_key_mapping(key))
 
     data_sources = tuple(provider + RandomLocation() for provider in data_providers)
 
-    pipeline = data_sources + RandomProvider()
-    if store_inputs_node:
-        pipeline += store_inputs_node
+    pipeline = data_sources + RandomProvider() + Duplicate(duplicates)
+
     for augmentation in augmentations:
         pipeline += augmentation
-    pipeline += output_node
-    print(request)
-    print(pipeline)
 
     with build(pipeline) as b:
-        b.request_batch(request)
+        return b.request_batch(request)
--- visualize_augmentations_paintera.py
+++ visualize_augmentations_paintera.py
@@ -6,15 +6,15 @@ import numpy as np
 
 import gpn.util
 import jnius_config
-from gunpowder import Hdf5Source, Roi, Hdf5Write, Coordinate
+from gunpowder import Hdf5Source, Roi, Coordinate
 
 RAW       = gpn.util.RAW
 GT_LABELS = gpn.util.GT_LABELS
 
 import argparse
-import h5py
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--max-heap-size", default="80g")
+parser.add_argument("--max-heap-size", default="16g")
 args = parser.parse_args()
 
 data_providers = []
@@ -24,8 +24,6 @@ file_pattern = 'sample_A_padded_20160501-2-additional-sections-fixed-offset.h5'
 
 
 for data in glob.glob(os.path.join(data_dir, file_pattern)):
-    print(data)
-    # TODO add masks later on
     h5_source = Hdf5Source(
         data,
         datasets={
@@ -41,29 +39,13 @@ offset = (13640, 10932, 10932)
 
 input_roi = Roi(offset=(0, 0, 0), shape=Coordinate((200, 3072, 3072)) * input_resolution)
 output_roi = Roi(offset=(120, -36, -36), shape=Coordinate((373, 1250, 1250)) * output_resolution)
-roi = Roi(offset=(0, 0, 0), shape=Coordinate((12, 10, 10)) * output_resolution)#shape=(7200, 648, 648))
-output_dir = os.path.join(os.path.expanduser("~"), "tmp")
-output_filename = "test-nodes.h5"
-dataset_names = {RAW: 'volumes/raw-augmented', GT_LABELS: 'volumes/labels/neuron_ids-downsampled-augmented'}
-input_dataset_names = {RAW: 'volumes/raw', GT_LABELS: 'volumes/labels/neuron_ids-downsampled'}
+roi = Roi(offset=(0, 0, 0), shape=Coordinate((120, 100, 100)) * output_resolution)#shape=(7200, 648, 648))
 
-output_node = Hdf5Write(
-    dataset_names=dataset_names,
-    output_dir=output_dir,
-    output_filename=output_filename)
-
-store_input_node = Hdf5Write(
-    dataset_names=input_dataset_names,
-    output_dir=output_dir,
-    output_filename=output_filename)
-
-gpn.util.run_augmentations(
+batch = gpn.util.run_augmentations(
     data_providers=data_providers,
     roi=lambda key: roi,
     augmentations=(),
-    keys_with_sizes=((RAW, input_roi), (GT_LABELS, output_roi)),
-    output_node=output_node,
-    store_inputs_node=None)
+    keys_with_sizes=((RAW, input_roi), (GT_LABELS, output_roi)))
 
 jnius_config.add_options('-Xmx{}'.format(args.max_heap_size))
 
@@ -76,30 +58,21 @@ PainteraBaseView = autoclass('org.janelia.saalfeldlab.paintera.PainteraBaseView'
 viewer = PainteraBaseView.defaultView()
 pbv = viewer.baseView
 scene, stage = payntera.jfx.start_stage(viewer.paneWithStatus.getPane())
+payntera.jfx.invoke_on_jfx_application_thread(lambda: pbv.orthogonalViews().setScreenScales([0.3, 0.1, 0.03]))
 
 
-with h5py.File(os.path.join(output_dir, output_filename)) as f:
-    raw = f[input_dataset_names[RAW]].value
-    raw_augmented = f[dataset_names[RAW]].value
-    labels = f[input_dataset_names[GT_LABELS]].value
-    labels_augmented = f[dataset_names[GT_LABELS]].value
+raw = batch[RAW]
+labels = batch[GT_LABELS]
 
-raw_img           = imglyb.to_imglib(raw)
-raw_augmented_img = imglyb.to_imglib(raw_augmented)
+raw_img    = imglyb.to_imglib(raw.data)
+labels_img = imglyb.to_imglib(labels.data)
 
-labels_img           = imglyb.to_imglib(labels)
-labels_augmented_img = imglyb.to_imglib(labels_augmented)
+max_label = np.max(labels.data)
 
-max_label = np.max(labels)
-
-raw_state = pbv.addSingleScaleRawSource(raw_img, input_resolution[::-1], input_roi.get_begin()[::-1], np.min(raw), np.max(raw), 'raw')
-label_state = pbv.addSingleScaleLabelSource(labels_img, output_resolution[::-1], output_roi.get_begin()[::-1], max_label+1, 'labels')
-raw_augmented_state = pbv.addSingleScaleRawSource(raw_augmented_img, input_resolution[::-1], input_roi.get_begin()[::-1], np.min(raw_augmented), np.max(raw_augmented), 'raw-augmented')
-label_augmented_state = pbv.addSingleScaleLabelSource(labels_augmented_img, output_resolution[::-1], output_roi.get_begin()[::-1], max_label+1, 'labels-augmented')
+raw_state = pbv.addSingleScaleRawSource(raw_img, input_resolution[::-1], input_roi.get_begin()[::-1], np.min(raw.data), np.max(raw.data), 'raw-augmented')
+label_state = pbv.addSingleScaleLabelSource(labels_img, output_resolution[::-1], output_roi.get_begin()[::-1], max_label+1, 'labels-augmented')
 viewer.keyTracker.installInto(scene)
 scene.addEventFilter(autoclass('javafx.scene.input.MouseEvent').ANY, viewer.mouseTracker)
 
-hidden_state = payntera.jfx.WaitForHidden(stage=stage)
-
-while not hidden_state.is_hidden:
+while not stage.isHidden():
     time.sleep(0.1)
